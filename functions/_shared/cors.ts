eed356fea22fb5146b0b86de77d9f7e40fb3881f
Metais Bastien <bastienmetais79@gmail.com>
2024-11-25 18:00:19 +0100

    Ajout des url accepté (CORS)

--- functions/_shared/cors.ts
+++ functions/_shared/cors.ts
@@ -1,6 +1,18 @@
-export const corsHeaders = {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Credentials': 'true',
-}
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://latroupecastor.fr',
+    'https://latroupecastor.com'
+];
+
+export const corsHeaders = (requestOrigin?: string) => {
+    const origin = requestOrigin && allowedOrigins.indexOf(requestOrigin) !== -1
+        ? requestOrigin
+        : allowedOrigins[0];
+
+    return {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Credentials': 'true',
+    }
+};
